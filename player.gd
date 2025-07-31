57d58c89a81e23e994d55be05c9a7e41a6675ef4
Ben Bingham <ben@benbingham.ca>
2025-07-30 21:47:02 -0600

    Added jumping and falling

--- player.gd
+++ player.gd
@@ -2,15 +2,16 @@ extends CharacterBody3D
 
 @export var lookSensitivity = 0.001;
 
-@export var speed = 15.0;
-@export var jumpSpeed = 45.0;
-@export var gravity = 10.0;
+@export var groundSpeed = 15.0;
+@export var airSpeed = groundSpeed * 0.75;
+@export var jumpSpeed = 30.0;
+@export var gravity = 100.0;
 
 var forward = Vector3(0.0, 0.0, -1.0);
 var right = Vector3(1.0 ,0.0, 0.0);
 
-enum State { IDLE, RUNNING, FALLING, JUMPING }
-var state = State.IDLE;
+enum State { IDLE, RUNNING, JUMPING, FALLING }
+var state = State.FALLING;
 
 func _ready():
 	motion_mode = CharacterBody3D.MOTION_MODE_GROUNDED;
@@ -26,49 +27,47 @@ func _input(event):
 		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-70.0), deg_to_rad(89.9));
 
 func _physics_process(dt):
-	var motion = Vector3();
 	match state:
 		State.IDLE:
-			if Input.is_action_pressed("move_forward"):
+			if Input.get_vector("move_left", "move_right", "move_backward", "move_forward"):
 				state = State.RUNNING
-			if Input.is_action_pressed("move_backward"):
-				state = State.RUNNING
-			if Input.is_action_pressed("move_left"):
-				state = State.RUNNING
-			if Input.is_action_pressed("move_right"):
-				state = State.RUNNING
-			# space -> jumping
+
+			if Input.is_action_pressed("jump"):
+				state = State.JUMPING;
+				
+			velocity = Vector3();
+			
 		State.RUNNING:
-			if (!Input.is_action_pressed("move_forward") && 
-				!Input.is_action_pressed("move_backward") &&
-				!Input.is_action_pressed("move_left") &&
-				!Input.is_action_pressed("move_right")):
+			if !Input.get_vector("move_left", "move_right", "move_backward", "move_forward"):
 				state = State.IDLE;
 				
-			if Input.is_action_pressed("move_forward"):
-				motion += forward * speed;
-			if Input.is_action_pressed("move_backward"):
-				motion -= forward * speed;
-			if Input.is_action_pressed("move_left"):
-				motion -= right * speed;
-			if Input.is_action_pressed("move_right"):
-				motion += right * speed;
-			# Release all of WASD -> Idle
-			# space -> jumping
-		State.FALLING:
-			# Apply Gravity
-			# hit the ground -> idle
-			pass
+			var input = Input.get_vector("move_left", "move_right", "move_forward", "move_backward");
+			var vy = velocity.y;
+			velocity.y = 0;
+			velocity = transform.basis * Vector3(input.x, 0.0, input.y) * groundSpeed;
+			velocity.y = vy;
+
+			if Input.is_action_pressed("jump"):
+				state = State.JUMPING;
+				
+			if !is_on_floor():
+				state = State.FALLING;
+				
 		State.JUMPING:
-			# motion vector is negative in Y -> falling
-			pass
+			velocity.y = jumpSpeed;
+			state = State.FALLING;
 
+		State.FALLING:
+			velocity.y -= gravity * dt;
+			
+			if is_on_floor():
+				state = State.IDLE;
+				
+			# In air movement
+			var input = Input.get_vector("move_left", "move_right", "move_forward", "move_backward");
+			var vy = velocity.y;
+			velocity.y = 0;
+			velocity = transform.basis * Vector3(input.x, 0.0, input.y) * airSpeed;
+			velocity.y = vy;
 
-	
-	#if Input.is_action_pressed("jump"):
-		#motion += up * dt * jumpSpeed;
-#
-	#motion -= up * gravity * dt;
-	velocity = motion;
-	print(velocity);
 	move_and_slide();
