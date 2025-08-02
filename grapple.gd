0412f070ffefd87aa5839fb5acbbba44d18651de
Ben Bingham <ben@benbingham.ca>
2025-08-02 13:53:19 -0600

    Added ball to grapple point

--- grapple.gd
+++ grapple.gd
@@ -6,18 +6,24 @@ var grappling = false;
 
 func startGrapple():
 	grappling = true;
+	var hook = $Hook;
+	hook.show();
 
 func stopGrapple():
 	grappling = false;
+	var hook = $Hook;
+	hook.hide();
 
 func _ready():
 	player.connect("start_grapple", startGrapple);
 	player.connect("stop_grapple", stopGrapple);
 
-func _process(dt):
+func _process(_dt):
 	mesh.clear_surfaces();
 
 	if grappling:
+		var hook = $Hook;
+		hook.position = to_local(get_parent().get_parent().get_parent().grapplePoint);
 		mesh.surface_begin(Mesh.PRIMITIVE_LINES);
 
 		mesh.surface_set_color(Color.RED);
